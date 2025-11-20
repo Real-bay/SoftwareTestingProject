b0c5e77c9582a59d7ba91bf10d68644eab662c82
Jyri Määttä <jyri.maatta@tuni.fi>
2025-11-20 17:28:07 +0200

    Create isEmpty.js test suite

---
+++ tests/isEmpty.js.test.jsx
@@ -0,0 +1,24 @@
+// isEmpty.js.test.jsx
+// Manual test suite for isEmpty.js
+
+import { describe, it, expect } from "vitest";
+import isEmpty from "../src/isEmpty.js";
+
+describe("isEmpty.js - test suite", () => {
+
+    it("TC024 - returns true for null and undefined", () => {
+        expect(isEmpty(null)).toBe(true);
+        expect(isEmpty(undefined)).toBe(true);
+    });
+
+    it("TC025 - returns false for non-empty arrays", () => {
+        expect(isEmpty(["Jyri", "Matias"])).toBe(false);
+        expect(isEmpty("x")).toBe(false);
+    });
+
+    it("TC026 - returns true for empty objects", () => {
+        expect(isEmpty([])).toBe(true);
+        expect(isEmpty("")).toBe(true);
+        expect(isEmpty({})).toBe(true);
+    });
+});
