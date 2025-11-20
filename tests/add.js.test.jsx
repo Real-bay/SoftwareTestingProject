535199687b146fc87885d098930b8b63367add59
Jyri Määttä <jyri.maatta@tuni.fi>
2025-11-20 13:50:29 +0200

    Create TC001
    - added also isSymbol.js because it is needed to run the add-function

--- tests/add.js.test.jsx
+++ tests/add.js.test.jsx
@@ -1,1 +1,15 @@
-// Sample file for unit tests
+// add.js.test.jsx
+// Unit tests for add.js
+// The tests validate arithmetic behavior and type handling.
+// These tests follow the Phase 1 manually designed test plan.
+
+import { describe, it, expect } from "vitest";
+import add from "../src/add.js";
+
+describe("add.js test suite", () => {
+
+    it("TC001 - Add two whole numbers", () => {
+        expect(add(1,1)).toBe(2);
+        expect(add(347,128)).toBe(475);
+    })
+})
