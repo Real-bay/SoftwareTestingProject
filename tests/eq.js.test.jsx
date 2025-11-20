0f2a5af05772dfe92d59a6bb787c06f871265a66
Jyri Määttä <jyri.maatta@tuni.fi>
2025-11-20 14:30:21 +0200

    Create TC013

--- tests/eq.js.test.jsx
+++ tests/eq.js.test.jsx
@@ -16,4 +16,9 @@ describe("eq.js test suite", () => {
     it("TC012 - Compare coercion inputs", () => {
         expect(eq(1, "1")).toBe(true);
     })
+
+    it("TC013 - Compare undefined/null values", () => {
+        expect(eq(null, null)).toBe(true);
+        expect(eq(undefined, null)).toBe(false);
+    })
 })
