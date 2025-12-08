78e31bcbd43a76dc7f05f09a295e0bf8cbdd708c
Matias Aitolahti <96495033+Real-bay@users.noreply.github.com>
2025-12-08 17:04:46 +0200

    Add ESLint + Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default [
+  js.configs.recommended,
+  prettierRecommended,
+  {
+    files: ['tests/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ['src/', 'build/', 'dist/', 'node_modules/'],
+  },
+];
